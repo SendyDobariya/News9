79ae99dfa8df99725b8a0e2e174ae61232d62c63
SendyDobariya <sendydobariya@gmail.com>
2022-07-12 09:45:40 +0530

    add validation in api , add next and previous button

--- src/components/News.js
+++ src/components/News.js
@@ -7,19 +7,51 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: true
+      loading: true,
+      page: 1,
     }
   }
 
   async componentDidMount() {
-    let url = 'https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e8666daa6ea7430b8deda5d4f61fff6f';
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e8666daa6ea7430b8deda5d4f61fff6f&page=${this.state.page}&pageSize=20`;
     let response = await fetch(url);
     let json = await response.json();
     console.log(json);
     this.setState({
       articles: json.articles,
-      loading: false
+      loading: false,
+      totalArticals: json.totalResults,
+
     })
+  }
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e8666daa6ea7430b8deda5d4f61fff6f&page=${this.state.page - 1}&pageSize=20`;
+    let response = await fetch(url);
+    let json = await response.json();
+    console.log(json);
+    this.setState({
+      articles: json.articles,
+      loading: false,
+      page: this.state.page - 1,
+    })
+  }
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math
+      .ceil(this.state.totalArticals / 20)) {
+
+    } else {
+
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e8666daa6ea7430b8deda5d4f61fff6f&page=${this.state.page + 1}&pageSize=20`;
+      let response = await fetch(url);
+      let json = await response.json();
+      console.log(json);
+      this.setState({
+        articles: json.articles,
+        loading: false,
+        page: this.state.page + 1,
+      })
+    }
+
   }
   render() {
     return (
@@ -29,9 +61,15 @@ export class News extends Component {
 
           {this.state.articles.map((data) => {
             return <div className='col-md-4' key={data.url}>
-              <NewsItem title={data.title} description={data.description.slice(0.30)} urlToImage={data.urlToImage} url={data.url} />
+              <NewsItem title={data.title} description={data.description} urlToImage={data.urlToImage} url={data.url} />
             </div>
           })}
+        </div>
+        <div className='container d-flex justify-content-between'>
+          <button disabled={this.state.page <= 1} type='button' className='btn btn-dark' onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button disabled={this.state.page + 1 > Math
+            .ceil(this.state.totalArticals / 20)} type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
+
         </div>
       </div>
     )
